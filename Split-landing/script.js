3168d8499bd755801c2dbe726f52edf972f21c05
Edwin Mwangi <edwinmwangi138@gmail.com>
2022-02-22 18:06:50 +0300

    for smaller screens

--- Split-landing/script.js
+++ Split-landing/script.js
@@ -4,15 +4,20 @@ const container = document.querySelector('.container');
 
 left.addEventListener('mouseenter', () =>{
     container.classList.add('hover-left')
+    container.classList.add('hover-top')
+
 })
 left.addEventListener('mouseleave', () =>{
     container.classList.remove('hover-left')
+    container.classList.remove('hover-top')
 })
 
 right.addEventListener('mouseenter', () =>{
     container.classList.add('hover-right')
+    container.classList.add('hover-bottom')
 })
 
 right.addEventListener('mouseleave', () =>{
     container.classList.remove('hover-right')
+    container.classList.remove('hover-bottom')
 })
