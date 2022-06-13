9d2082b065ab4707fb7109e53137b4c5898be4e2
Edwin Mwangi <edwinmwangi138@gmail.com>
2022-06-14 00:36:43 +0300

    dad jokes the js

---
+++ dad jokes/script.js
@@ -0,0 +1,24 @@
+//we'll be fetching from icanhazdadkoke.com
+//there iss a small manual...showing how to fetch data from API...curl for terminal(you can check this)
+//brad show us about a program called postman...its a GUI...easier to understand how fetch works...02:18
+//ther is also a library called axios that Brad prefers over fetch(but it's 3rd party)
+const joke = document.getElementById('joke')
+const jokeBtn = document.getElementById('getjoke')
+
+jokeBtn.addEventListener('click', generateJoke)//don't include brackets in the arg generateJoke
+
+generateJoke()
+
+function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json'
+        }
+    }
+
+    fetch('https://icanhazdadjoke.com/', config)
+        .then(res => res.json())
+        .then(data => {
+            joke.innerText = data.joke
+        })
+}
